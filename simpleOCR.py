c36799bf797a3ae1907769479d685cbfc52ee4ea
AndyFromTaiwan <chuangp@student.unimelb.edu.au>
2020-08-04 20:26:20 +1000

    add searchable pdf conversion

--- simpleOCR.py
+++ simpleOCR.py
@@ -7,9 +7,13 @@ except ImportError:
 import pytesseract
 
 
+# Configs
 img_path = 'image'
-text_path = 'text'
+txt_path = 'txt'
+pdf_path = 'pdf'
 language = 'eng'
+GEN_PDF = True
+
 
 files = listdir(img_path)
 
@@ -18,13 +22,22 @@ for f in files:
     try: 
         (filename, extension) = splitext(f)
         img = Image.open(join(img_path, f))
-        content = pytesseract.image_to_string(img, lang=language)
 
-        text = join(text_path, filename+'.txt')
-        tf = open(text, "w")
-        tf.write(content)
-        tf.close()
-        print('Successfully wrote the converted text in', text)
+        # Simple image to string
+        content = pytesseract.image_to_string(img, lang=language)
+        txt = join(txt_path, filename+'.txt')
+        with open(txt, 'w') as tf:
+            tf.write(content)
+        print('Successfully wrote the converted text in', txt)
+
+        # Get a searchable PDF
+        if GEN_PDF:
+            pdf = join(pdf_path, filename+'.pdf')
+            content = pytesseract.image_to_pdf_or_hocr(img, lang=language, extension='pdf')
+            with open(pdf, 'w+b') as pf:
+                pf.write(content) # pdf type is bytes by default
+            print('Successfully wrote the converted pdf in', pdf)
+
     except Exception as e:
         print("Fail!", e.__class__, "occurred:")
         print(e)
